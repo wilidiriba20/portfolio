ad224001632df5dc52d46c3f53b7e0eff89d985a
wilidiriba20 <157874486+wilidiriba20@users.noreply.github.com>
2026-04-02 19:52:45 +0300

    Add mobile hamburger menu

--- portfolio.js
+++ portfolio.js
@@ -20,3 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
