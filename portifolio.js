4a542420f5812bbe1232e2e1a7ce39da76e9250b
wilidiriba20 <157874486+wilidiriba20@users.noreply.github.com>
2026-04-02 19:19:37 +0300

    Init observer on DOMContentLoaded and unobserve

--- portifolio.js
+++ portifolio.js
@@ -1,16 +1,22 @@
-const observerOptions = {
-  threshold: 0.15,
-  rootMargin: "0px 0px -50px 0px",
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = {
+    threshold: 0.1, 
+    rootMargin: "0px 0px -100px 0px", 
+  };
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("reveal");
-    }
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("reveal");
+        observer.unobserve(entry.target); 
+      }
+    });
+  }, observerOptions);
+
+  const firstSection = document.querySelector("section");
+  firstSection.classList.add("reveal");
+
+  document.querySelectorAll("section").forEach((section) => {
+    observer.observe(section);
   });
-}, observerOptions);
-
-document.querySelectorAll("section").forEach((section) => {
-  observer.observe(section);
 });
